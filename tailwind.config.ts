ab8d12f2bd79773d597b36b09b00efdb71392aae
Glen Ochieng <glenochieng045@gmail.com>
2025-01-17 16:03:58 +0300

    chore: added custom colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                // Neutral
+                "very-light-gray": "hsl(228,33%,97%)",
+                "light-gray": "hsl(223,19%,93%)",
+                "grayish-blue": "hsl(211, 10%, 45%)",
+                "dark-blue": "hsl(212, 24%, 26%)",
+                // Primary
+                "moderate-blue": "hsl(238, 40%, 52%)",
+                "soft-red": "hsl(358, 79%, 66%)",
+                "light-grayish-blue": "hsl(239, 57%, 85%)",
+                "pale-red": "hsl(357, 100%, 86%)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
