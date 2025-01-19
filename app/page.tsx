5a46fcbc16e12349ceb3b97bdcc14c2c7a2792d4
Glen Ochieng <glenochieng045@gmail.com>
2025-01-19 06:40:17 +0300

    feat: added mock comment data

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import MessageCard from "@/components/MessageCard";
+import CommentCard from "@/components/CommentCard";
+import {comments, User} from "@/mock-data/data";
 
 export default function Home() {
+    const user: User = {
+        "image": {
+            "png": "/images/avatars/image-juliusomo.png",
+            "webp": "/images/avatars/image-juliusomo.webp"
+        },
+        "username": "juliusomo"
+    }
     return (
-        <div className={"p-4"}>
-            <MessageCard/>
+        <div className={"p-4 flex flex-col h-full"}>
+            <div className={"flex flex-col gap-4"}>
+                {comments.map((comment) => (
+                    <CommentCard key={comment.id} user={user} comment={comment}/>
+                ))}
+            </div>
         </div>
     );
 }
--- components/CommentCard.tsx
+++ components/CommentCard.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import Image from "next/image";
+import type {Comment, User} from "@/mock-data/data"
 
-function MessageCard() {
+
+type CommentCardProps = {
+    user: User,
+    comment: Comment;
+}
+
+function CommentCard({user, comment}: CommentCardProps) {
     return (
         <div className={"bg-white p-4 rounded-xl"}>
             <div className={"flex items-center gap-6"}>
                 <Image
-                    src={"/images/avatars/image-amyrobson.png"}
-                    alt={"amyrobson profile"}
+                    src={comment.user.image.png}
+                    alt={`${comment.user.username} profile`}
                     width={40}
                     height={40}/>
-                <span className={"font-bold"}>amyrobson</span>
+                {user.username == comment.user.username &&
+                    <span className={"p-2 bg-moderate-blue text-white"}>You</span>}
+                <span className={"font-bold"}>{comment.user.username}</span>
                 <span className={"text-grayish-blue font-light"}>1 month ago</span>
             </div>
-            <div className={"py-4 text-grayish-blue"}>
-                Impressive! Though it seems the drag feature could be improved. But overall it looks incredible.
-                You&#39;ve nailed the design and the responsiveness at various breakpoints works really well.
-            </div>
+            <div className={"py-4 text-grayish-blue"}>{comment.content}</div>
             <div className={"flex justify-between items-center"}>
                 <div className={"flex bg-light-gray items-center gap-4 rounded-lg py-2 px-4"}>
                     <button>
                         <Image src={"/images/icon-minus.svg"} alt={"down-vote"} height={10} width={10}/>
                     </button>
-                    <span className={"text-moderate-blue font-[500]"}>12</span>
+                    <span className={"text-moderate-blue font-[500]"}>{comment.score}</span>
                     <button>
                         <Image src={"/images/icon-plus.svg"} alt={"up-vote"} height={10} width={10}/>
                     </button>
@@ -36,4 +42,4 @@ function MessageCard() {
     );
 }
 
-export default MessageCard;
+export default CommentCard;
