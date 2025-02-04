775618c3a9ab04d566cb4d22837927e95f5987de
Glen Ochieng <glenochieng045@gmail.com>
2025-02-04 07:31:48 +0300

    chore: changed indentation of textarea

--- components/CommentInput.tsx
+++ components/CommentInput.tsx
@@ -17,17 +17,16 @@ export default function CommentInput({user, addComment}: CommentInputProps) {
         setCommentInput("");
     }
 
-
     return <div className={"flex flex-col  gap-8 bg-white rounded-xl p-4 mt-4"}>
-                    <textarea
-                        className={"h-24 border-2 rounded-xl p-4 border-grayish-blue"}
-                        name={"comment"}
-                        placeholder={"Add a comment..."}
-                        value={commentInput}
-                        onChange={(event) => {
-                            setCommentInput(event.target.value);
-                        }}
-                    />
+        <textarea
+            className={"h-24 border-2 rounded-xl p-4 border-grayish-blue"}
+            name={"comment"}
+            placeholder={"Add a comment..."}
+            value={commentInput}
+            onChange={(event) => {
+                setCommentInput(event.target.value);
+            }}
+        />
         <div className={"flex justify-between items-center gap-4"}>
             <Image src={user.image.png} alt={`${user.username} profile avatar`} height={40} width={40}/>
             <button className={"bg-moderate-blue  text-white rounded-lg disabled:bg-grayish-blue"}
