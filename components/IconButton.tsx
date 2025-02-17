f054743eaef1d89bb561f7a4653e490eb993473c
Glen Ochieng <glenochieng045@gmail.com>
2025-02-17 05:35:33 +0300

    fix: added width and height props

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -4,16 +4,18 @@ import Image from "next/image";
 type IconButtonProps = {
     label: string;
     iconPath: string;
+    height?: number;
+    width?: number;
     onClick?: () => void;
     iconHeight?: number;
     iconWidth?: number;
     labelClassName?: string;
 }
 
-function IconButton({onClick, label, iconPath, labelClassName}: IconButtonProps) {
+function IconButton({onClick, label, height, width, iconPath, labelClassName}: IconButtonProps) {
     return (
         <button className={"flex items-center gap-2"} onClick={onClick}>
-            <Image src={iconPath} alt={""} height={15} width={15}/>
+            <Image src={iconPath} alt={""} height={height ?? 15} width={width ?? 15}/>
             <span className={`font-[500] ${labelClassName ? labelClassName : ""}`}>{label}</span>
         </button>
     );
