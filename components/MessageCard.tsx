1bb62785719502c8f6daf9c9393ea9d6a900fc88
Glen Ochieng <glenochieng045@gmail.com>
2025-03-01 10:53:07 +0300

    feat: added functionality to Header buttons

--- components/MessageCard.tsx
+++ components/MessageCard.tsx
@@ -17,7 +17,7 @@ type MessageCardProps = {
 
 function MessageCard({user, comment, reply, className}: MessageCardProps) {
     const [replyInputIsVisible, setReplyInputIsVisible] = useState(false)
-    const {addReply, onEditComment, onEditReply} = useContext(AppContext)
+    const {addReply, onEditComment, onEditReply, openDeleteModal} = useContext(AppContext)
     const [isEditing, setIsEditing] = useState(false)
 
     const editor = useEditor({
@@ -31,37 +31,61 @@ function MessageCard({user, comment, reply, className}: MessageCardProps) {
         immediatelyRender: false
     })
 
+    function toggleReplyInputVisibility() {
+        setReplyInputIsVisible(prev => !prev)
+    }
+
+    function onUpdateMessage() {
+        if (!editor?.getText()) {
+            return
+        }
+        if (!reply) {
+            onEditComment(editor.getText(), comment.id)
+        } else {
+            onEditReply(editor.getText(), comment.id, reply.id)
+        }
+        setIsEditing(false)
+    }
+
+    function canUpdateMessage() {
+        return isEditing && (editor !== null && editor.getText().length > 0)
+    }
+
+    function toggleIsEditing() {
+        setIsEditing(prev => !prev)
+    }
+
     return (
         <div className={`${className ?? ""}`}>
             <div className={"flex items-center gap-4 bg-white p-4 rounded-xl"}>
                 <ScoreWidget score={reply ? reply.score : comment.score} commentId={comment.id} replyId={reply?.id}
                              className={"w-12 flex-grow-0 flex-shrink-0 flex-col items-center rounded-xl max-h-max hidden sm:flex"}/>
                 <div className={"w-full"}>
-                    <Header author={reply ? reply.user : comment.user}/>
+                    <Header author={reply ? reply.user : comment.user}
+                            toggleReplyInputVisibility={toggleReplyInputVisibility}
+                            isEditing={isEditing}
+                            toggleIsEditing={toggleIsEditing}
+                            onDelete={() => openDeleteModal(comment.id, reply?.id)}
+                    />
                     {isEditing ? (
-                        <>
-                            <EditorContent editor={editor}/>
-                        </>
+                        <div className={"flex gap-4 items-start"}>
+                            <EditorContent editor={editor} className={"flex-1"}/>
+                            <button
+                                className={"hidden sm:block mt-4 px-4 py-2 bg-moderate-blue  text-white rounded-lg disabled:bg-grayish-blue"}
+                                onClick={onUpdateMessage}
+                                disabled={!canUpdateMessage()}>Update
+                            </button>
+                        </div>
                     ) : <Content content={reply ? reply.content : comment.content}/>}
                     <Footer commentId={comment.id}
                             replyId={reply?.id}
                             author={reply ? reply.user : comment.user}
                             score={reply ? reply.score : comment.score}
-                            toggleReplyInputVisibility={() => setReplyInputIsVisible(prev => !prev)}
+                            toggleReplyInputVisibility={toggleReplyInputVisibility}
                             isEditing={isEditing}
-                            onUpdate={() => {
-                                if (!editor?.getText()) {
-                                    return
-                                }
-                                if (!reply) {
-                                    onEditComment(editor.getText(), comment.id)
-                                } else {
-                                    onEditReply(editor.getText(), comment.id, reply.id)
-                                }
-                                setIsEditing(false)
-                            }}
-                            canUpdate={isEditing && (editor !== null && editor.getText().length > 0)}
-                            toggleIsEditing={() => setIsEditing(prev => !prev)}
+                            onUpdate={onUpdateMessage}
+                            canUpdate={canUpdateMessage()}
+                            toggleIsEditing={toggleIsEditing}
                             className={"sm:hidden"}
                     />
                 </div>
@@ -86,10 +110,14 @@ function MessageCard({user, comment, reply, className}: MessageCardProps) {
 }
 
 type HeaderProps = {
-    author: User
+    author: User,
+    isEditing: boolean,
+    toggleReplyInputVisibility: () => void,
+    toggleIsEditing?: () => void,
+    onDelete?: () => void,
 }
 
-function Header({author}: HeaderProps) {
+function Header({author, toggleIsEditing, toggleReplyInputVisibility, onDelete}: HeaderProps) {
     const {user} = useContext(AppContext);
 
     return <div className={"flex items-center justify-between w-full"}>
@@ -110,13 +138,11 @@ function Header({author}: HeaderProps) {
                         <IconButton label={"Delete"} iconPath={"/images/icon-delete.svg"}
                                     height={14} width={12}
                                     labelClassName={"text-soft-red"}
-                                    onClick={() => {
-                                    }}/>
+                                    onClick={onDelete}/>
                         <IconButton label={"Edit"} iconPath={"/images/icon-edit.svg"}
                                     height={14} width={14}
                                     labelClassName={"text-moderate-blue"}
-                                    onClick={() => {
-                                    }}
+                                    onClick={toggleIsEditing}
                         />
                     </div>
                 ) : (
@@ -125,8 +151,7 @@ function Header({author}: HeaderProps) {
                         iconPath={"/images/icon-reply.svg"}
                         height={13} width={14}
                         labelClassName={"text-moderate-blue"}
-                        onClick={() => {
-                        }}
+                        onClick={toggleReplyInputVisibility}
                     />
                 )}
         </div>
