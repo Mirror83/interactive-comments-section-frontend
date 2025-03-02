b2264e4a21fc89f5b34dbb289696db33b73cb4a5
Glen Ochieng <glenochieng045@gmail.com>
2025-03-02 15:13:47 +0300

    refactor: created VoteButton component to remove repetition in className declaration
    this could be refactored such that the VoteButton component can hve the VoteType as the only required argument though (this could be done later)

--- components/ScoreWidget.tsx
+++ components/ScoreWidget.tsx
@@ -9,29 +9,42 @@ type ScoreWidgetProps = {
     className?: string;
 }
 
+type VoteButtonProps = {
+    imageSrc: string, imageAlt: string, onClick: () => void, disabled: boolean
+}
+
 function ScoreWidget({score, commentId, replyId, className}: ScoreWidgetProps) {
     const {voteMessage} = useContext(AppContext)
     const [currentVoteType, setCurrentVoteType] = useState<VoteType>()
 
+    function vote(voteType: VoteType) {
+        setCurrentVoteType(voteType)
+        voteMessage(commentId, replyId, voteType)
+    }
+
+    function VoteButton({imageSrc, imageAlt, onClick, disabled}: VoteButtonProps) {
+        return <button onClick={onClick}
+                       disabled={disabled}
+                       className={"w-full flex flex-col items-center justify-center p-2"}>
+            <Image src={imageSrc} alt={imageAlt} height={3} width={11}/>
+        </button>
+    }
+
     return (
         <div className={`flex bg-light-gray items-center gap-4 rounded-lg py-2 px-2 ${className ?? ""}`}>
-            <button onClick={() => {
-                setCurrentVoteType(VoteType.DOWN_VOTE)
-                voteMessage(commentId, replyId, VoteType.DOWN_VOTE)
-            }}
-                    disabled={currentVoteType == VoteType.DOWN_VOTE}
-                    className={"w-full flex flex-col items-center p-2 justify-center"}>
-                <Image src={"/images/icon-minus.svg"} alt={"down-vote"} height={3} width={11}/>
-            </button>
+            <VoteButton
+                imageSrc={"images/icon-plus.svg"}
+                imageAlt={"upvote"}
+                onClick={() => vote(VoteType.UP_VOTE)}
+                disabled={currentVoteType == VoteType.UP_VOTE}
+            />
             <span className={"text-moderate-blue font-[500]"}>{score}</span>
-            <button onClick={() => {
-                setCurrentVoteType(VoteType.UP_VOTE)
-                voteMessage(commentId, replyId, VoteType.UP_VOTE)
-            }}
-                    disabled={currentVoteType == VoteType.UP_VOTE}
-                    className={"w-full flex flex-col items-center justify-center p-2"}>
-                <Image src={"/images/icon-plus.svg"} alt={"up-vote"} height={11} width={11}/>
-            </button>
+            <VoteButton
+                imageSrc={"images/icon-minus.svg"}
+                imageAlt={"downvote"}
+                onClick={() => vote(VoteType.DOWN_VOTE)}
+                disabled={currentVoteType == VoteType.DOWN_VOTE}
+            />
         </div>
     );
 }
