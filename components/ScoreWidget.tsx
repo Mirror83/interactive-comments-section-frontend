1365ce84c48e65bdb1644f4476f687e8efaeee98
Glen Ochieng <glenochieng045@gmail.com>
2025-03-02 13:43:39 +0300

    fix: removed ability to vote unlimitedly in either side
    the behaviour that this fix causes, however, does not allow one to change their minds; if they up-vote initially, down-voting will just take the votes to where they were initially and vice versa

--- components/ScoreWidget.tsx
+++ components/ScoreWidget.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import AppContext, {VoteType} from "@/context/app-context";
 import Image from "next/image";
 
@@ -11,15 +11,24 @@ type ScoreWidgetProps = {
 
 function ScoreWidget({score, commentId, replyId, className}: ScoreWidgetProps) {
     const {voteMessage} = useContext(AppContext)
+    const [currentVoteType, setCurrentVoteType] = useState<VoteType>()
 
     return (
         <div className={`flex bg-light-gray items-center gap-4 rounded-lg py-2 px-2 ${className ?? ""}`}>
-            <button onClick={() => voteMessage(commentId, replyId, VoteType.DOWN_VOTE)}
+            <button onClick={() => {
+                setCurrentVoteType(VoteType.DOWN_VOTE)
+                voteMessage(commentId, replyId, VoteType.DOWN_VOTE)
+            }}
+                    disabled={currentVoteType == VoteType.DOWN_VOTE}
                     className={"w-full flex flex-col items-center p-2 justify-center"}>
                 <Image src={"/images/icon-minus.svg"} alt={"down-vote"} height={3} width={11}/>
             </button>
             <span className={"text-moderate-blue font-[500]"}>{score}</span>
-            <button onClick={() => voteMessage(commentId, replyId, VoteType.UP_VOTE)}
+            <button onClick={() => {
+                setCurrentVoteType(VoteType.UP_VOTE)
+                voteMessage(commentId, replyId, VoteType.UP_VOTE)
+            }}
+                    disabled={currentVoteType == VoteType.UP_VOTE}
                     className={"w-full flex flex-col items-center justify-center p-2"}>
                 <Image src={"/images/icon-plus.svg"} alt={"up-vote"} height={11} width={11}/>
             </button>
