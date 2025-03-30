222f0346e3fa794dd89fe8af1d2bd83a594f3a83
Glen Ochieng <glenochieng045@gmail.com>
2025-03-31 00:12:24 +0300

    chore: added placeholder brightness hover pseudoclass

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React from "react";
 
 type ActionButtonProps = {
-    onClick?: () => void;
-    action: "confirm" | "cancel";
-    children?: React.ReactNode;
-}
+  onClick?: () => void;
+  action: "confirm" | "cancel";
+  children?: React.ReactNode;
+};
 
-function ActionButton({onClick, action, children}: ActionButtonProps) {
-    return (
-        <button
-            className={`py-3 min-w-[150px] text-white rounded-lg ${action === "confirm" ? "bg-soft-red" : "bg-grayish-blue"}`}
-            onClick={onClick}>
-            {children}
-        </button>
-    );
+function ActionButton({ onClick, action, children }: ActionButtonProps) {
+  return (
+    <button
+      className={`py-3 min-w-[150px] text-white rounded-lg 
+            hover:brightness-110 ${
+              action === "confirm" ? "bg-soft-red" : "bg-grayish-blue"
+            }`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default ActionButton;
--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -14,7 +14,7 @@ type IconButtonProps = {
 
 function IconButton({onClick, label, height, width, iconPath, labelClassName}: IconButtonProps) {
     return (
-        <button className={"flex items-center gap-2"} onClick={onClick}>
+        <button className={"flex items-center gap-2 hover:brightness-125"} onClick={onClick}>
             <Image src={iconPath} alt={""} height={height ?? 15} width={width ?? 15}/>
             <span className={`font-[500] ${labelClassName ? labelClassName : ""}`}>{label}</span>
         </button>
